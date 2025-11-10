5063dd52ad771cbd834f7a1f9ccd4e20f98abb3f
ordinaryman-dev <a726889227@163.com>
2025-11-10 14:56:12 +0800

    Delete model directory

--- model/Movable.java
+++
@@ -1,6 +0,0 @@
-// Movable接口（规范移动行为）
-package com.ucd.frogger.model;
-public interface Movable {
-    void calculateMove(double deltaTime); // 计算单帧移动后的坐标
-    void updatePosition(); // 更新对象实际坐标（计算后调用）
-}
